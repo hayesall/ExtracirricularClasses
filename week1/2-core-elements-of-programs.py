d27b318c1e179590db87ff692c4d19f83311b6df
Alexander L. Hayes <alexander@batflyer.net>
2017-09-05 16:16:39 -0500

    adding solutions to some intermediary problems.

---
+++ week1/2-core-elements-of-programs.py
@@ -0,0 +1,20 @@
+'''
+MITx 6.00.1x
+Introduction to Computer Science and Programming Using Python
+
+Week 1: Python Basics
+2. Core Elements of Programs
+'''
+
+# Exercise: hello world
+
+print('hello world')
+
+# Exercise: happy
+#     Write a piece of Python code that prints the string 'hello world' if the value
+#     of an integer variable, happy, is strictly greater than 2.
+
+happy = 3
+
+if happy > 2:
+    print('hello world')
