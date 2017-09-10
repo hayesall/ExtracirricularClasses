e9c9e881abeeae82f4b2c2712e2120b0427da702
batflyer <alexander@batflyer.net>
2017-09-10 14:51:58 -0500

    short section on higher-order functions and decorators

--- DataScience/week3/review.py
+++ DataScience/week3/review.py
@@ -22,10 +22,10 @@ class LambdaDemo:
     print(my_func(5))
 
 class MapDemo:
-    '''Map:'''
-
-    # Transform function: "let's write a method called 'transform' that takes a list
-    # and a function as parameters and applies the function to each element of the list.
+    '''
+    Transform function: let's write a method called 'transform' that takes a
+    list a function as parameters and applies the function to each list item.
+    '''
     
     x = [1, 2, 3]
     def mult_2(x):
@@ -35,15 +35,13 @@ class MapDemo:
         for i in range(len(arr)):
             arr[i] = func(arr[i])
 
-    transform(x, mult_2)
-    #print(map(mult_2, [1, 2, 3]))
+    transform(x, mult_2) # Notice that this is a backwards version of map.
     print(x)
 
 class HigherOrderDemo:
     '''Defining functions within functions.'''
     
-    # def within def
-    
+    # Define functions inside functions.
     def greet(name):
         def get_message():
             return "Hello "
@@ -53,8 +51,7 @@ class HigherOrderDemo:
 
     print(greet("John"))
 
-    # Functions can be passed as parameters to other functions
-
+    '''Functions can be passed as parameters to other functions.'''
     def greet(name):
         return "Hello " + name
     def call_func(func):
@@ -62,8 +59,7 @@ class HigherOrderDemo:
         return func(other_name)
     print(call_func(greet))
 
-    # Functions can return other functions:
-
+    '''Functions can return other functions:'''
     def compose_greet_func():
         def get_message():
             return "Hello there!"
@@ -72,3 +68,32 @@ class HigherOrderDemo:
     greet = compose_greet_func()
     print(greet())
 
+    '''Assign functions to variables:'''
+    def greet(name):
+        return "hello " + name
+    greet_someone = greet
+    print(greet_someone("JOHN!"))
+
+    '''Inner functions have access to the enclosing scope.'''
+    def compose_greet_func(name):
+        def get_message():
+            return "Hello there "+name+"!"
+        return get_message
+
+    greet = compose_greet_func('Jack')
+    print(greet())
+
+class DecoratorDemo:
+    '''Composition of decorators.'''
+
+    '''This is a cool way to create wrapper functions.'''
+    def p_decorate(func):
+        def func_wrapper(name):
+            return "<p>{0}</p>".format(func(name))
+        return func_wrapper
+
+    @p_decorate
+    def get_text(name):
+        return "lorem ipsum, {0} dolor sit amet".format(name)
+
+    print(get_text("John"))
