c951313abc3e09ccf2248b4df3f7416fe32bb02b
batflyer <alexander@batflyer.net>
2017-09-10 15:28:22 -0500

    Added two exercises.

--- DataScience/week3/review.py
+++ DataScience/week3/review.py
@@ -97,3 +97,74 @@ class DecoratorDemo:
         return "lorem ipsum, {0} dolor sit amet".format(name)
 
     print(get_text("John"))
+    get_text = p_decorate(get_text)
+
+
+# A global definition for p_decorate, showing that it can be used within
+# the following 'Person' class.
+def p_decorate(func):
+    def func_wrapper(self):
+        return "<p>{0}</p>".format(func(self))
+    return func_wrapper
+    
+class Person(object):
+    '''
+    In Python, methods are functions that expect their first parameter to be a
+    reference to the current object
+    '''
+    def __init__(self):
+        self.name = "John"
+        self.family = "Doe"
+    
+    @p_decorate
+    def get_fullname(self):
+        return self.name + ' ' + self.family
+
+my_person = Person()
+print(my_person.get_fullname())
+
+'''
+Exercise 1:
+
+We have a class defined for vehicles. Create two new vehicles (car1 and car2).
+Set car1 to be a red convertible worth $60,000.00 with a name of Fer, and car2
+to be a blue van named Jump worth $10,000.00
+'''
+
+# Define the vehicle class
+class Vehicle:
+
+    def __init__(self, name, kind, color, value):
+        self.name = name
+        self.kind = kind
+        self.color = color
+        self.value = value
+    
+    def description(self):
+        desc_str = "%s is a %s %s worth $%.2f." % \
+                   (self.name, self.color, self.kind, self.value)
+        return desc_str
+
+car1 = Vehicle("Fer", "red", "Convertible", 60000.00)
+car2 = Vehicle("Jump", "blue", "Van", 10000.00)
+
+print(car1.description())
+print(car2.description())
+
+# Exercise 2:
+class A(object):
+    def foo(self, x):
+        print("Executing foo(%s,%s)", (self, x))
+    @classmethod
+    def class_foo(cls, x):
+        print("Executing class_foo(%s,%s)", (cls, x))
+    @staticmethod
+    def static_foo(x):
+        print("Executing static_foo(%s)", x)
+
+a = A()
+a.foo(1)
+a.class_foo(1)
+A.class_foo(1)
+a.static_foo(1)
+A.static_foo(1)
